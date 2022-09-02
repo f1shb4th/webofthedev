bb0ae07bbba55b0eb4a0f8662ffba86b963b36e8
ave <averyamack@Gmail.com>
2022-09-02 08:07:54 -0400

    webofthedev

---
+++ funny js shit/funny.js
@@ -0,0 +1,32 @@
+//die die die dide died eided esdi esd 
+console.log('AAAAAAAGH!! FFFUCK!!!!! FUCK, SHIT, AND OTHER SUCH EXPLECTIVES!! AAAAAAAKK!');
+
+let bawlz = "beenas";
+console.log(bawlz);
+
+let f = 'grubble', u = 'beebple'; //wtf
+
+const yourmom = 69;
+console.log(yourmom+1);
+
+let gayPeople = false;
+let no = null;
+
+let gah = {
+    isGay: true,
+    name: 'gomp'
+};
+console.log(gah);
+console.log(gah.name);
+
+let yourSwag = [];
+
+let yassCore = ['ya', 'as'];
+console.log(yassCore[0]+yassCore[1]);
+yassCore[2] = 'ss';
+
+function fuckballs() {
+    console.log('blurmp')
+}
+
+fuckballs();
