37df99f5b6b1fa1f5635eb99bed870c82ea6bc93
ave <averyamack@Gmail.com>
2022-09-04 17:21:16 -0400

    webofthedev

---
+++ FirstJS/silly.js
@@ -0,0 +1,18 @@
+function alert1(){
+    alert("YOU CAN'T HIDE");
+}
+function alert2(){
+    alert("I KNOW WHERE YOU LIVE");
+}
+function console1(){
+    console.log("GET GOT");
+}
+function console2(){
+    console.log("L BOZO");
+}
+function changeBackground(){
+
+}
+function changeButton(){
+    
+}
