0b0576ee8783a9a001bd411d3d02d5b7c500b4f2
ave <averyamack@Gmail.com>
2022-09-05 09:56:08 -0400

    fgysjhdvbcmnx

--- FirstJS/silly.js
+++ FirstJS/silly.js
@@ -14,5 +14,15 @@ function changeBackground(){
     document.body.classList.add("flash");
 }
 function changeButton(){
-    document.div.classList.replace("box", "box2");
+    document.body.div.classList.replace("box", "box2");
+}
+function The(){
+    while(true){
+        alert1();
+        alert2();
+        console1();
+        console2();
+        changeBackground();
+        changeButton();
+    }
 }
