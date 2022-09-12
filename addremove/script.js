5a096ec7047c3fa6db2ca3c053e9b2a8bd0baef3
ave <averyamack@Gmail.com>
2022-09-12 09:05:44 -0400

    web dev

--- addremove/script.js
+++ addremove/script.js
@@ -1,9 +1,9 @@
-let frogList = ["red pumpkin toadlet","desert rain frog", "eastern leopard frog","fire bellied toad","purple harliquin toad","grey tree frog"]
+let frogList = ["red pumpkin toadlet","desert rain frog", "eastern leopard frog","fire bellied toad","purple harliquin toad","grey tree frog","fowler's toad","mexican burrowing toad",]
 
 let froptionsEl = document.querySelector("#froptions");
 
 for (let el of frogList){
     let listFrog = document.createElement("li");
     listFrog.textContent=el;
-    optionsEl.appendChild(listFrog);
+    froptionsEl.appendChild(listFrog);
 }
