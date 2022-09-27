446a4c03a18f6388d520e9837ea64f6d5e747691
ave <averyamack@Gmail.com>
2022-09-27 09:10:43 -0400

    web dev

--- nodeserver/fucksdigns.js
+++ nodeserver/fucksdigns.js
@@ -16,25 +16,40 @@ function htmlS(res){
     </head>
     <body>`);}
 
+function KILLEVERYONETILTHEYDIE(res, people){
+    for(let x of people){
+        res.write(`<p>fuck off ${x.lN} die die die die ${x.fN}</p>`)
+    }
+}
+
 function htmlE(res){
     res.write(`</body>
     </html>`);
     res.end();}
 
+let people = []
+
 app.get('/',(req,res)=>{
-people.push({fN:req.body.firstName, lN:req.body.lastName})
+people.push({fN:req.query.firstName, lN:req.query.lastName})
     console.log(req.query);
     htmlS(res);
     res.write(`
-            <form method="GET" action="http://localhost:3000">
-                <input type="text" name="firstName" id="fname">
-                <input type="submit" value="Go">
-            </form>
-            <p>fuck off ${req.query.lastName} die die die die ${req.query.firstName}</p>
+    <form method="GET" action="http://localhost:3000">
+        <div>   
+        <label for="fname">who tf r you???????????????????</label>
+        <input type="text" name="firstName" id="fname">
+        </div>
+        <div>
+        hi :3
+        <input type="text" name="lastName" id="lname">
+        <input type="submit" value="Go">
+        </div>
+    </form>
             `);
+    KILLEVERYONETILTHEYDIE(res, people);
     htmlE(res);
 })
 
 app.listen(port,()=>{
-    console.log(`piss your pants die ddie die die die on port${port}`)
+    console.log(`piss your pants die die die die die on port${port}`)
 })
