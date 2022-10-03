d2785fe5e30514c4f04e39bd05fe060e14b9a490
ave <averyamack@Gmail.com>
2022-10-02 20:14:49 -0400

    web dev

--- login/login.js
+++ login/login.js
@@ -1,4 +1,3 @@
-console.log("DIE DDIE DIE DIE DEATH DIE DIE DIE");
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -7,14 +6,11 @@ app.use(express.urlencoded({extended:false}))
 app.use(express.static('public'));
 app.post("/register",(req,res)=>{
     console.log("new dude");
+    res.redirect('/superSecretSite.html');
 })
 app.post("/login",(req,res)=>{
-    console.log("old dude")
+    console.log("old dude");
 })
-
-
-
-
 
 app.listen(port,()=>{
     console.log(`piss your pants and die on port${port}`)
