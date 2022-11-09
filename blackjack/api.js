e68bd63dd53a229ed3d8a45b42bb49cc33f76442
ave <averyamack@Gmail.com>
2022-11-09 08:37:42 -0500

    web dev

---
+++ blackjack/api.js
@@ -0,0 +1,23 @@
+const e = require('express');
+const express = require('express');
+const { restart } = require('nodemon');
+const app = express();
+const port = 6969;
+
+
+function htmlS(res, pageName){
+    res.write(`<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title> ${pageName} </title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>`);}
+
+function htmlE(res){
+    res.write(`</body>
+    </html>`);
+    res.end();}
