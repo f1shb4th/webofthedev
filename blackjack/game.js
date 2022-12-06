f5d0815ae7e8f970ed0f0a1ae9f160c2e4573410
ave <averyamack@Gmail.com>
2022-12-06 16:54:30 -0500

    web dev

--- blackjack/game.js
+++ blackjack/game.js
@@ -6,64 +6,6 @@ const port = 6969;
 
 app.use(express.static('public'))
 
-let deck = [
-    {colour:"black", suit:"club", value:"ace",printval:[0,0,2,0,1]},
-    {colour:"black", suit:"club", value:2,printval:[1,0,0,0,3]},
-    {colour:"black", suit:"club", value:3,printval:[1,0,2,0,3]},
-    {colour:"black", suit:"club", value:4,printval:[4,0,0,0,4]},
-    {colour:"black", suit:"club", value:5,printval:[4,0,2,0,4]},
-    {colour:"black", suit:"club", value:6,printval:[4,0,4,0,4]},
-    {colour:"black", suit:"club", value:7,printval:[4,2,4,0,4]},
-    {colour:"black", suit:"club", value:8,printval:[4,2,4,2,4]},
-    {colour:"black", suit:"club", value:9,printval:[5,0,5,0,5]},
-    {colour:"black", suit:"club", value:10,printval:[4,5,0,5,4]},
-    {colour:"black", suit:"club", value:"jack",printval:[2,0,2,0,3]},
-    {colour:"black", suit:"club", value:"queen",printval:[4,0,2,0,5]},
-    {colour:"black", suit:"club", value:"king",printval:[6,0,2,0,7]},
-
-    {colour:"black", suit:"spade", value:"ace",printval:[0,0,2,0,1]},
-    {colour:"black", suit:"spade", value:2,printval:[1,0,0,0,3]},
-    {colour:"black", suit:"spade", value:3,printval:[1,0,2,0,3]},
-    {colour:"black", suit:"spade", value:4,printval:[4,0,0,0,4]},
-    {colour:"black", suit:"spade", value:5,printval:[4,0,2,0,4]},
-    {colour:"black", suit:"spade", value:6,printval:[4,0,4,0,4]},
-    {colour:"black", suit:"spade", value:7,printval:[4,2,4,0,4]},
-    {colour:"black", suit:"spade", value:8,printval:[4,2,4,2,4]},
-    {colour:"black", suit:"spade", value:9,printval:[5,0,5,0,5]},
-    {colour:"black", suit:"spade", value:10,printval:[4,5,0,5,4]},
-    {colour:"black", suit:"spade", value:"jack",printval:[2,0,2,0,3]},
-    {colour:"black", suit:"spade", value:"queen",printval:[4,0,2,0,5]},
-    {colour:"black", suit:"spade", value:"king",printval:[6,0,2,0,7]},
-
-    {colour:"red", suit:"heart", value:"ace",printval:[0,0,2,0,1]},
-    {colour:"red", suit:"heart", value:2,printval:[1,0,0,0,3]},
-    {colour:"red", suit:"heart", value:3,printval:[1,0,2,0,3]},
-    {colour:"red", suit:"heart", value:4,printval:[4,0,0,0,4]},
-    {colour:"red", suit:"heart", value:5,printval:[4,0,2,0,4]},
-    {colour:"red", suit:"heart", value:6,printval:[4,0,4,0,4]},
-    {colour:"red", suit:"heart", value:7,printval:[4,2,4,0,4]},
-    {colour:"red", suit:"heart", value:8,printval:[4,2,4,2,4]},
-    {colour:"red", suit:"heart", value:9,printval:[5,0,5,0,5]},
-    {colour:"red", suit:"heart", value:10,printval:[4,5,0,5,4]},
-    {colour:"red", suit:"heart", value:"jack",printval:[2,0,2,0,3]},
-    {colour:"red", suit:"heart", value:"queen",printval:[4,0,2,0,5]},
-    {colour:"red", suit:"heart", value:"king",printval:[6,0,2,0,7]},
-
-    {colour:"red", suit:"diamond", value:"ace",printval:[0,0,2,0,1]},
-    {colour:"red", suit:"diamond", value:2,printval:[1,0,0,0,3]},
-    {colour:"red", suit:"diamond", value:3,printval:[1,0,2,0,3]},
-    {colour:"red", suit:"diamond", value:4,printval:[4,0,0,0,4]},
-    {colour:"red", suit:"diamond", value:5,printval:[4,0,2,0,4]},
-    {colour:"red", suit:"diamond", value:6,printval:[4,0,4,0,4]},
-    {colour:"red", suit:"diamond", value:7,printval:[4,2,4,0,4]},
-    {colour:"red", suit:"diamond", value:8,printval:[4,2,4,2,4]},
-    {colour:"red", suit:"diamond", value:9,printval:[5,0,5,0,5]},
-    {colour:"red", suit:"diamond", value:10,printval:[4,5,0,5,4]},
-    {colour:"red", suit:"diamond", value:"jack",printval:[2,0,2,0,3]},
-    {colour:"red", suit:"diamond", value:"queen",printval:[4,0,2,0,5]},
-    {colour:"red", suit:"diamond", value:"king",printval:[6,0,2,0,7]},
-];
-
 let shuffledDeck = [];
 
 let game = {
@@ -95,6 +37,63 @@ function gameMessage(game){
 };
 
 function shuffleDeck(){
+    let deck = [
+        {colour:"black", suit:"club", value:"ace",printval:[0,0,2,0,1]},
+        {colour:"black", suit:"club", value:2,printval:[1,0,0,0,3]},
+        {colour:"black", suit:"club", value:3,printval:[1,0,2,0,3]},
+        {colour:"black", suit:"club", value:4,printval:[4,0,0,0,4]},
+        {colour:"black", suit:"club", value:5,printval:[4,0,2,0,4]},
+        {colour:"black", suit:"club", value:6,printval:[4,0,4,0,4]},
+        {colour:"black", suit:"club", value:7,printval:[4,2,4,0,4]},
+        {colour:"black", suit:"club", value:8,printval:[4,2,4,2,4]},
+        {colour:"black", suit:"club", value:9,printval:[5,0,5,0,5]},
+        {colour:"black", suit:"club", value:10,printval:[4,5,0,5,4]},
+        {colour:"black", suit:"club", value:"jack",printval:[2,0,2,0,3]},
+        {colour:"black", suit:"club", value:"queen",printval:[4,0,2,0,5]},
+        {colour:"black", suit:"club", value:"king",printval:[6,0,2,0,7]},
+    
+        {colour:"black", suit:"spade", value:"ace",printval:[0,0,2,0,1]},
+        {colour:"black", suit:"spade", value:2,printval:[1,0,0,0,3]},
+        {colour:"black", suit:"spade", value:3,printval:[1,0,2,0,3]},
+        {colour:"black", suit:"spade", value:4,printval:[4,0,0,0,4]},
+        {colour:"black", suit:"spade", value:5,printval:[4,0,2,0,4]},
+        {colour:"black", suit:"spade", value:6,printval:[4,0,4,0,4]},
+        {colour:"black", suit:"spade", value:7,printval:[4,2,4,0,4]},
+        {colour:"black", suit:"spade", value:8,printval:[4,2,4,2,4]},
+        {colour:"black", suit:"spade", value:9,printval:[5,0,5,0,5]},
+        {colour:"black", suit:"spade", value:10,printval:[4,5,0,5,4]},
+        {colour:"black", suit:"spade", value:"jack",printval:[2,0,2,0,3]},
+        {colour:"black", suit:"spade", value:"queen",printval:[4,0,2,0,5]},
+        {colour:"black", suit:"spade", value:"king",printval:[6,0,2,0,7]},
+    
+        {colour:"red", suit:"heart", value:"ace",printval:[0,0,2,0,1]},
+        {colour:"red", suit:"heart", value:2,printval:[1,0,0,0,3]},
+        {colour:"red", suit:"heart", value:3,printval:[1,0,2,0,3]},
+        {colour:"red", suit:"heart", value:4,printval:[4,0,0,0,4]},
+        {colour:"red", suit:"heart", value:5,printval:[4,0,2,0,4]},
+        {colour:"red", suit:"heart", value:6,printval:[4,0,4,0,4]},
+        {colour:"red", suit:"heart", value:7,printval:[4,2,4,0,4]},
+        {colour:"red", suit:"heart", value:8,printval:[4,2,4,2,4]},
+        {colour:"red", suit:"heart", value:9,printval:[5,0,5,0,5]},
+        {colour:"red", suit:"heart", value:10,printval:[4,5,0,5,4]},
+        {colour:"red", suit:"heart", value:"jack",printval:[2,0,2,0,3]},
+        {colour:"red", suit:"heart", value:"queen",printval:[4,0,2,0,5]},
+        {colour:"red", suit:"heart", value:"king",printval:[6,0,2,0,7]},
+    
+        {colour:"red", suit:"diamond", value:"ace",printval:[0,0,2,0,1]},
+        {colour:"red", suit:"diamond", value:2,printval:[1,0,0,0,3]},
+        {colour:"red", suit:"diamond", value:3,printval:[1,0,2,0,3]},
+        {colour:"red", suit:"diamond", value:4,printval:[4,0,0,0,4]},
+        {colour:"red", suit:"diamond", value:5,printval:[4,0,2,0,4]},
+        {colour:"red", suit:"diamond", value:6,printval:[4,0,4,0,4]},
+        {colour:"red", suit:"diamond", value:7,printval:[4,2,4,0,4]},
+        {colour:"red", suit:"diamond", value:8,printval:[4,2,4,2,4]},
+        {colour:"red", suit:"diamond", value:9,printval:[5,0,5,0,5]},
+        {colour:"red", suit:"diamond", value:10,printval:[4,5,0,5,4]},
+        {colour:"red", suit:"diamond", value:"jack",printval:[2,0,2,0,3]},
+        {colour:"red", suit:"diamond", value:"queen",printval:[4,0,2,0,5]},
+        {colour:"red", suit:"diamond", value:"king",printval:[6,0,2,0,7]},
+    ];
     let shuffledDeck = [];
     for(let card of deck){
         cNum = Math.floor(Math.random() * deck.length);
@@ -154,6 +153,7 @@ restartGame();
 
 app.post('/hit',(req,res)=>{
     dealCard(shuffledDeck,game.playerHand);
+    console.log(game.playerHand)
     game.pHandVal = handVal(game.playerHand);
     if(game.pHandVal>21){
         game.playerBust=true;
@@ -180,6 +180,7 @@ app.post('/restart',(req,res)=>{
     restartGame();
     game.gMsg=gameMessage(game);
     res.json(game);
+    console.log(game)
 });
 
 function htmlS(res, pageName){
